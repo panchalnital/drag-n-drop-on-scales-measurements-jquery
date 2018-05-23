57f78a830beba534efa6309afe735d2168125233
panchalnital <panchalnital5@gmail.com>
2018-05-23 13:06:32 +0530

    drag and drop scale measurement  functionality
    scale measurement for 0 to 100 for you have particular image drag on scale on ranking for define on parity wise image on drag and drop after values measurement

---
+++ js/meter.js
@@ -0,0 +1,112 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function xycoordinate(pleft) {
+    
+    if(pleft>=183 && pleft <=191){ return 1;}
+    else if(pleft>=192 && pleft <=199){return 2;}
+    else if(pleft>=200 && pleft <=207){return 3;}
+    else if(pleft>=208 && pleft <=215){return 4;}
+    else if(pleft>=216 && pleft <=223){return 5;}
+    else if(pleft>=224 && pleft <=231){return 6;}
+    else if(pleft>=232 && pleft <=239){return 7;}
+    else if(pleft>=240 && pleft <=247){return 8;}
+    else if(pleft>=248 && pleft <=255){return 9;}
+    else if(pleft>=256 && pleft <=263){return 10;}
+    else if(pleft>=264 && pleft <=271){return 11;}
+    else if(pleft>=272 && pleft <=279){return 12;}
+    else if(pleft>=280 && pleft <=287){return 13;}
+    else if(pleft>=288 && pleft <=295){return 14;}
+    else if(pleft>=296 && pleft <=303){return 15;}
+    else if(pleft>=304 && pleft <=311){return 16;}
+    else if(pleft>=312 && pleft <=319){return 17;}
+    else if(pleft>=320 && pleft <=327){return 18;}
+    else if(pleft>=328 && pleft <=335){return 19;}
+    else if(pleft>=336 && pleft <=343){return 20;}
+    else if(pleft>=344 && pleft <=351){return 21;}
+    else if(pleft>=352 && pleft <=359){return 22;}
+    else if(pleft>=360 && pleft <=367){return 23;}
+    else if(pleft>=368 && pleft <=375){return 24;}
+    else if(pleft>=376 && pleft <=383){return 25;}
+    else if(pleft>=384 && pleft <=391){return 26;}
+    else if(pleft>=392 && pleft <=399){return 27;}
+    else if(pleft>=400 && pleft <=407){return 28;}
+    else if(pleft>=408 && pleft <=415){return 29;}
+    else if(pleft>=416 && pleft <=423){return 30;}
+    else if(pleft>=424 && pleft <=431){return 31;}
+    else if(pleft>=432 && pleft <=439){return 32;}
+    else if(pleft>=440 && pleft <=447){return 33;}
+    else if(pleft>=448 && pleft <=455){return 34;}
+    else if(pleft>=456 && pleft <=463){return 35;}
+    else if(pleft>=464 && pleft <=471){return 36;}
+
+    else if(pleft>=472 && pleft <=479){return 38;}
+    else if(pleft>=480 && pleft <=487){return 39;}
+    else if(pleft>=488 && pleft <=495){return 40;}
+    else if(pleft>=496 && pleft <=511){return 41;}
+    else if(pleft>=512 && pleft <=519){return 42;}
+    else if(pleft>=520 && pleft <=527){return 43;}
+    else if(pleft>=528 && pleft <=535){return 44;}
+    else if(pleft>=536 && pleft <=543){return 45;}
+    else if(pleft>=544 && pleft <=521){return 46;}
+    else if(pleft>=552 && pleft <=559){return 47;}
+    else if(pleft>=560 && pleft <=567){return 48;}
+    else if(pleft>=668 && pleft <=575){return 49;}
+    else if(pleft>=576 && pleft <=583){return 50;}
+    else if(pleft>=584 && pleft <=591){return 51;}
+    else if(pleft>=592 && pleft <=599){return 52;}
+    else if(pleft>=600 && pleft <=607){return 53;}
+    else if(pleft>=608 && pleft <=615){return 54;}
+    else if(pleft>=616 && pleft <=623){return 55;}
+    else if(pleft>=624 && pleft <=631){return 56;}
+    else if(pleft>=632 && pleft <=639){return 57;}
+    else if(pleft>=640 && pleft <=647){return 58;}
+    else if(pleft>=648 && pleft <=655){return 59;}
+    else if(pleft>=656 && pleft <=663){return 60;}
+    else if(pleft>=664 && pleft <=671){return 61;}
+    else if(pleft>=672 && pleft <=679){return 62;}
+    else if(pleft>=680 && pleft <=687){return 63;}
+    else if(pleft>=688 && pleft <=695){return 64;}
+    else if(pleft>=696 && pleft <=703){return 65;}
+    else if(pleft>=704 && pleft <=711){return 66;}
+    else if(pleft>=712 && pleft <=719){return 67;}
+    else if(pleft>=720 && pleft <=727){return 68;}
+    else if(pleft>=728 && pleft <=735){return 69;}
+    else if(pleft>=736 && pleft <=743){return 70;}
+    else if(pleft>=744 && pleft <=751){return 71;}
+    else if(pleft>=752 && pleft <=759){return 72;}
+    else if(pleft>=760 && pleft <=767){return 73;}
+    else if(pleft>=768 && pleft <=775){return 74;}
+    else if(pleft>=776 && pleft <=783){return 75;}
+    else if(pleft>=784 && pleft <=791){return 76;}
+    else if(pleft>=792 && pleft <=799){return 77;}
+    else if(pleft>=880 && pleft <=807){return 78;}
+    else if(pleft>=808 && pleft <=815){return 79;}
+    else if(pleft>=816 && pleft <=823){return 80;}
+    else if(pleft>=824 && pleft <=831){return 81;}
+    else if(pleft>=432 && pleft <=439){return 82;}
+    else if(pleft>=840 && pleft <=847){return 83;}
+    else if(pleft>=848 && pleft <=855){return 84;}
+    else if(pleft>=856 && pleft <=863){return 85;}
+    else if(pleft>=864 && pleft <=871){return 86;}
+    else if(pleft>=872 && pleft <=879){return 87;}
+    else if(pleft>=880 && pleft <=887){return 88;}
+    else if(pleft>=888 && pleft <=865){return 89;}
+    else if(pleft>=896 && pleft <=903){return 90;}
+    else if(pleft>=904 && pleft <=911){return 91;}
+    else if(pleft>=912 && pleft <=919){return 92;}
+    else if(pleft>=920 && pleft <=927){return 93;}
+    else if(pleft>=928 && pleft <=935){return 94;}
+    else if(pleft>=936 && pleft <=943){return 95;}
+    else if(pleft>=944 && pleft <=951){return 96;}
+    else if(pleft>=952 && pleft <=959){return 97;}
+    else if(pleft>=960 && pleft <=967){return 98;}
+    else if(pleft>=968 && pleft <=975){return 99;}
+    else if(pleft>=976 && pleft <=1000){return 100;}
+ 
+}
+
+
